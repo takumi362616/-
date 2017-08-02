7778e12d74b4efdff450e4d7f988b0d54ccc0af3
ビデオエイペックス <apex@17MMGF2Si090720.local>
2017-08-02 13:44:38 +0900

    Initial Commit

---
+++ 小野大輔がかっこいい件について/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  小野大輔がかっこいい件について
+//
+//  Created by ビデオエイペックス on 2017/08/02.
+//  Copyright © 2017 takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
